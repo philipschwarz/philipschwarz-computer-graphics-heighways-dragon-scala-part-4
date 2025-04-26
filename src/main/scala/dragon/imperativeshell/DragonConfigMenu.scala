74b6608b30c15c52d07e51acac8a18dbf2400d3c
philipschwarz <philip.johann.schwarz@gmail.com>
2025-04-26 20:50:43 +0100

    change keys used for commands

--- src/main/scala/dragon/imperativeshell/DragonConfigMenu.scala
+++ src/main/scala/dragon/imperativeshell/DragonConfigMenu.scala
@@ -8,14 +8,14 @@ import java.awt.{Menu, MenuItem, MenuShortcut}
 class DragonConfigMenu(actionListener: ActionListener) extends Menu("Configuration"):
 
   private val menuItemDetails = List(
-    ConfigChange.GrowOlder -> KeyEvent.VK_RIGHT,
-    ConfigChange.GrowYounger -> KeyEvent.VK_LEFT,
-    ConfigChange.GrowLarger -> KeyEvent.VK_UP,
-    ConfigChange.GrowSmaller -> KeyEvent.VK_DOWN,
-    ConfigChange.MoveRight -> KeyEvent.VK_CLOSE_BRACKET,
-    ConfigChange.MoveLeft -> KeyEvent.VK_OPEN_BRACKET,
-    ConfigChange.MoveUp -> KeyEvent.VK_EQUALS,
-    ConfigChange.MoveDown -> KeyEvent.VK_MINUS,
+    ConfigChange.GrowOlder -> KeyEvent.VK_CLOSE_BRACKET,
+    ConfigChange.GrowYounger -> KeyEvent.VK_OPEN_BRACKET,
+    ConfigChange.GrowLarger -> KeyEvent.VK_EQUALS,
+    ConfigChange.GrowSmaller -> KeyEvent.VK_MINUS,
+    ConfigChange.MoveRight -> KeyEvent.VK_RIGHT,
+    ConfigChange.MoveLeft -> KeyEvent.VK_LEFT,
+    ConfigChange.MoveUp -> KeyEvent.VK_UP,
+    ConfigChange.MoveDown -> KeyEvent.VK_DOWN,
     ConfigChange.ChangeColour -> KeyEvent.VK_C,
     ConfigChange.ChangeOrientation -> KeyEvent.VK_O
   )
